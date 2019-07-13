9a42dc30d488399288fc0ed7a7804e4e2fa81d63
risyandi <risyandi.94@gmail.com>
2019-07-14 04:56:36 +0700

    finishing create-dynamic-pages

--- next-learn-demo/3-create-dynamic-pages/pages/index.js
+++ next-learn-demo/3-create-dynamic-pages/pages/index.js
@@ -1,9 +1,24 @@
-import Layout from '../components/MyLayout.js'
+import Layout from '../components/MyLayout.js';
+import Link from 'next/link';
 
-export default function Index() {
+const PostLink = (props) => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+    <a>{props.title}</a>
+    </Link>
+  </li>
+);
+
+export default function Blog() {
   return (
     <Layout>
+      <h1>Blog</h1>
       <p>Hello Next.js</p>
+      <ul>
+        <PostLink title="Hello NextJs"/>
+        <PostLink title="Learn NextJs is awesome"/>
+        <PostLink title="deploy Apps with Zenit"/>
+      </ul>
     </Layout>
-  )
-}
+  );
+};

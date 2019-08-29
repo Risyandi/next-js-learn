a6099bfd417f105afbb7fed25bc07c72bf97ec60
risyandi <risyandi.94@gmail.com>
2019-08-29 14:39:09 +0700

    modified post

--- next-learn-demo/6-fetching-data/pages/post.js
+++ next-learn-demo/6-fetching-data/pages/post.js
@@ -1,13 +1,22 @@
-import { withRouter } from 'next/router'
-import Layout from '../components/MyLayout.js'
+import Layout from '../components/MyLayout.js';
+import fetch from 'isomorphic-unfetch';
 
-function Post(props) {
-  return (
-    <Layout>
-      <h1>{props.router.query.title}</h1>
-      <p>This is the blog post content.</p>
-    </Layout>
-  )
-}
+const Post = props => (
+  <Layout>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+    <img src={props.show.image.medium} />
+  </Layout>
+);
 
-export default withRouter(Post)
+Post.getInitialProps = async function (context) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show };
+};
+
+export default Post;

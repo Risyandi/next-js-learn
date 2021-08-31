4c429aeadfe06773e60838c76e9aeabbef821543
Risyandi <Risyandi.94@gmail.com>
2021-08-31 11:20:46 +0700

    fix: update nextjsLearn

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
+import Link from 'next/link';
+
 const Index = () => (
     <div>
-        <h1>Hello Next JS</h1>
+        <h1>Homepage Welcome Next JS</h1>
+        <div>
+            <Link href="/about">
+                <a>About</a>
+            </Link>
+        </div>
     </div>
 );
 
